88043aedf4c2a06ec002776d69f6c529320295c8
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-04-05 10:44:48 +0300

    Migrate the database to add the profile table

--- insta/models.py
+++ insta/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Profile(models.Model):
-    profilePhoto = models.ImageField(upload_to = 'profilephotos/')
+    profilePhoto = models.ImageField(upload_to = 'photos/')
     bio = models.TextField()
