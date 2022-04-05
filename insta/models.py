9259b1eb9182c12548977f74d80ed885f32c10bb
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-04-05 10:38:29 +0300

    Add the profile model

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    profilePhoto = models.ImageField(upload_to = 'profilephotos/')
+    bio = models.TextField()
