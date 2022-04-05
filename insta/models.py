a77eb786f78cdd14e994b6f8d2aa8a6509d56db4
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-04-05 10:51:46 +0300

    Create the image model

--- insta/models.py
+++ insta/models.py
@@ -1,6 +1,20 @@
+import profile
 from django.db import models
 
 # Create your models here.
 class Profile(models.Model):
     profilePhoto = models.ImageField(upload_to = 'photos/')
     bio = models.TextField()
+
+    def __str__(self):
+        return self.bio
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'photos/')
+    imageName = models.CharField(max_length = 30)
+    imageCaption = models.TextField()
+    # profile = models.ForeignKey(Profile)
+    comments = models.TextField()
+
+    def __str__(self):
+        return self.imageName
