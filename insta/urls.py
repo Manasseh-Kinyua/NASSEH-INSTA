0754bb320338837bd3bcde2376fbc2f02356a960
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-04-05 14:43:00 +0300

    Add authentication

--- insta/urls.py
+++ insta/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('login/', views.loginPage, name = 'login'),
+    path('logout/', views.logoutUser, name = 'logout'),
     path('', views.index, name = 'index'),
     path('image_details/<str:pk>/', views.image_details, name = 'image_details'),
     path('comment/', views.comment, name = 'comment'),
--- insta/views.py
+++ insta/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render, redirect
 from .models import Image
 from .forms import CommentForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
+def loginPage(request):
+    if request.method == 'Post':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username = username)
+        except:
+            messages.error(request, 'User does not exist')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Username or password does not exist')
+        
+    context = {}
+    return render(request, 'insta/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('index')
+
 def index(request):
     images = Image.objects.all()
     context = {
@@ -10,10 +37,12 @@ def index(request):
     }
     return render(request, 'insta/index.html', context)
 
+@login_required(login_url = '/login')
 def image_details(request, pk):
     image = Image.objects.get(id=pk)
     return render(request, 'insta/imagedetail.html', {"image":image})
 
+@login_required(login_url = '/login')
 def comment(request):
     form = CommentForm()
     if request.method == 'POST':
